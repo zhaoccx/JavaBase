a1b52d34b2be3b18242d745fce5a5d910f8032cf
zhaoccx <zhaoccx@hotmail.com>
2014-11-12 00:27:10 +0800

    ffd

--- JavaOne/src/com/s/Ctd.java
+++ JavaOne/src/com/s/Ctd.java
@@ -3,5 +3,8 @@ package com.s;
 public class Ctd {
 	public static void main(String[] args) {
 		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 }
