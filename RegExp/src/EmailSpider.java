f691de750760f233db3a963388c374834477565a
zhaoccx <zhaoccx@hotmail.com>
2014-11-13 11:36:58 +0800

    5252

--- RegExp/src/EmailSpider.java
+++ RegExp/src/EmailSpider.java
@@ -16,10 +16,8 @@ public class EmailSpider {
 			}
 			br.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
