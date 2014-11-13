2e078ba40fc699689c426e6c39d3446be457b169
zhaoccx <zhaoccx@hotmail.com>
2014-11-13 20:56:54 +0800

    ss

--- RegExp/src/Test.java
+++ RegExp/src/Test.java
@@ -1,14 +1,15 @@
 public class Test {
 
+	private static int count = 1;
+
 	public static void main(String[] args) {
-		// 简单认识正则表达式
-		// p("a2c".matches("..."));
+		// 简单认识正则表达式的概念
+
+		// p("abc".matches("..."));
 		// p("a8729a".replaceAll("\\d", "-"));
 		// Pattern p = Pattern.compile("[a-z]{3}");
-		// Matcher m = p.matcher("fghtytyytytytyt");
-		// while (m.find()) {
-		// p(m.group());
-		// }
+		// Matcher m = p.matcher("fgh");
+		// p(m.matches());
 		// p("fgha".matches("[a-z]{3}"));
 
 		// 初步认识. * + ?
@@ -16,105 +17,131 @@ public class Test {
 		// p("a".matches("."));
 		// p("aa".matches("aa"));
 		// p("aaaa".matches("a*"));
-		p("aaaa".matches("a+"));
-		p("".matches("a*"));
-		p("aaaa".matches("a?"));
+		// p("aaaa".matches("a+"));
+		// p("".matches("a*"));
+		// p("aaaa".matches("a?"));
 		// p("".matches("a?"));
 		// p("a".matches("a?"));
 		// p("214523145234532".matches("\\d{3,100}"));
 		// p("192.168.0.aaa".matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"));
 		// p("192".matches("[0-2][0-9][0-9]"));
 
-		// ��Χ
-		/*
-		 * p("a".matches("[abc]")); p("a".matches("[^abc]"));
-		 * p("A".matches("[a-zA-Z]")); p("A".matches("[a-z]|[A-Z]"));
-		 * p("A".matches("[a-z[A-Z]]")); p("R".matches("[A-Z&&[RFG]]"));
-		 */
+		// 范围
 
-		// ��ʶ\s \w \d \
-		/*
-		 * p(" \n\r\t".matches("\\s{4}")); p(" ".matches("\\S"));
-		 * p("a_8".matches("\\w{3}"));
-		 * p("abc888&^%".matches("[a-z]{1,3}\\d+[&^#%]+"));
-		 * p("\\".matches("\\\\"));
-		 */
+		// p("a".matches("[abc]"));
+		// p("a".matches("[^abc]"));
+		// p("A".matches("[a-zA-Z]"));
+		// p("A".matches("[a-z]|[A-Z]"));
+		// p("A".matches("[a-z[A-Z]]"));
+		// p("R".matches("[A-Z&&[RFG]]"));
+
+		// 认识\s \w \d \
+
+		// p(" \n\r\t".matches("\\s{4}"));
+		// p(" ".matches("\\S"));
+		// p("a_8".matches("\\w{3}"));
+		// p("abc888&^%".matches("[a-z]{1,3}\\d+[&^#%]+"));
+		// p("\\".matches("\\\\"));
 
 		// POSIX Style
 		// p("a".matches("\\p{Lower}"));
 
 		// boundary
-		/*
-		 * p("hello sir".matches("^h.*")); p("hello sir".matches(".*ir$"));
-		 * p("hello sir".matches("^h[a-z]{1,3}o\\b.*"));
-		 * p("hellosir".matches("^h[a-z]{1,3}o\\b.*")); //whilte lines
-		 * p(" \n".matches("^[\\s&&[^\\n]]*\\n$"));
-		 * 
-		 * p("aaa 8888c".matches(".*\\d{4}."));
-		 * p("aaa 8888c".matches(".*\\b\\d{4}."));
-		 * p("aaa8888c".matches(".*\\d{4}."));
-		 * p("aaa8888c".matches(".*\\b\\d{4}."));
-		 */
+
+		p("hello sir".matches("^h.*"));
+		p("hello sir".matches(".*ir$"));
+		p("hello sir".matches("^h[a-z]{1,3}o\\b.*"));
+		// p("hellosir".matches("^h[a-z]{1,3}o\\b.*")); // whilte lines
+		// p(" \n".matches("^[\\s&&[^\\n]]*\\n$"));
+		//
+		// p("aaa 8888c".matches(".*\\d{4}."));
+		// p("aaa 8888c".matches(".*\\b\\d{4}."));
+		// p("aaa8888c".matches(".*\\d{4}."));
+		// p("aaa8888c".matches(".*\\b\\d{4}."));
 
 		// email
 		// p("asdfasdfsafsf@dsdfsdf.com".matches("[\\w[.-]]+@[\\w[.-]]+\\.[\\w]+"));
 
 		// matches find lookingAt
-		/*
-		 * Pattern p = Pattern.compile("\\d{3,5}"); String s =
-		 * "123-34345-234-00"; Matcher m = p.matcher(s); p(m.matches());
-		 * m.reset(); p(m.find()); p(m.start() + "-" + m.end()); p(m.find());
-		 * p(m.start() + "-" + m.end()); p(m.find()); p(m.start() + "-" +
-		 * m.end()); p(m.find()); //p(m.start() + "-" + m.end());
-		 * p(m.lookingAt()); p(m.lookingAt()); p(m.lookingAt());
-		 * p(m.lookingAt());
-		 */
+
+		// Pattern p2 = Pattern.compile("\\d{3,5}");
+		// String s = "123-34345-234-00";
+		// Matcher m2 = p2.matcher(s);
+		// p(m2.matches());
+		// m2.reset();
+		// p(m2.find());
+		// p(m2.start() + "-" + m2.end());
+		// p(m2.find());
+		// p(m2.start() + "-" + m2.end());
+		// p(m2.find());
+		// p(m2.start() + "-" + m2.end());
+		// p(m2.find()); // p(m.start() + "-" + m.end());
+		// p(m2.lookingAt());
+		// p(m2.lookingAt());
+		// p(m2.lookingAt());
+		// p(m2.lookingAt());
 
 		// replacement
-		/*
-		 * Pattern p = Pattern.compile("java", Pattern.CASE_INSENSITIVE);
-		 * Matcher m =
-		 * p.matcher("java Java JAVa JaVa IloveJAVA you hateJava afasdfasdf");
-		 * StringBuffer buf = new StringBuffer(); int i=0; while(m.find()) {
-		 * i++; if(i%2 == 0) { m.appendReplacement(buf, "java"); } else {
-		 * m.appendReplacement(buf, "JAVA"); } } m.appendTail(buf); p(buf);
-		 */
+
+		// Pattern p3 = Pattern.compile("java", Pattern.CASE_INSENSITIVE);
+		// Matcher m3 =
+		// p3.matcher("java Java JAVa JaVa IloveJAVA you hateJava afasdfasdf");
+		// StringBuffer buf = new StringBuffer();
+		// int i = 0;
+		// while (m3.find()) {
+		// i++;
+		// if (i % 2 == 0) {
+		// m3.appendReplacement(buf, "java");
+		// } else {
+		// m3.appendReplacement(buf, "JAVA");
+		// }
+		// }
+		// m3.appendTail(buf);
+		// p(buf);
 
 		// group
-		/*
-		 * Pattern p = Pattern.compile("(\\d{3,5})([a-z]{2})"); String s =
-		 * "123aa-34345bb-234cc-00"; Matcher m = p.matcher(s); while(m.find()) {
-		 * p(m.group()); }
-		 */
+
+		// Pattern p4 = Pattern.compile("(\\d{3,5})([a-z]{2})");
+		// String s2 = "123aa-34345bb-234cc-00";
+		// Matcher m4 = p4.matcher(s2);
+		// while (m4.find()) {
+		// p(m4.group());
+		// }
 
 		// qulifiers
-		/*
-		 * Pattern p = Pattern.compile(".{3,10}+[0-9]"); String s =
-		 * "aaaa5bbbb68"; Matcher m = p.matcher(s); if(m.find()) p(m.start() +
-		 * "-" + m.end()); else p("not match!");
-		 */
+
+		// Pattern p5 = Pattern.compile(".{3,10}+[0-9]");
+		// String s3 = "aaaa5bbbb68";
+		// Matcher m5 = p5.matcher(s3);
+		// if (m5.find())
+		// p(m5.start() + "-" + m5.end());
+		// else
+		// p("not match!");
 
 		// non-capturing groups
-		/*
-		 * Pattern p = Pattern.compile(".{3}(?=a)"); String s = "444a66b";
-		 * Matcher m = p.matcher(s); while(m.find()) { p(m.group()); }
-		 */
+
+		// Pattern p6 = Pattern.compile(".{3}(?=a)");
+		// String s4 = "444a66b";
+		// Matcher m6 = p6.matcher(s4);
+		// while (m6.find()) {
+		// p(m6.group());
+		// }
 
 		// back refenrences
-		/*
-		 * Pattern p = Pattern.compile("(\\d(\\d))\\2"); String s = "122";
-		 * Matcher m = p.matcher(s); p(m.matches());
-		 */
 
-		// flags�ļ�д
-		// Pattern p = Pattern.compile("java", Pattern.CASE_INSENSITIVE);
+		// Pattern p7 = Pattern.compile("(\\d(\\d))\\2");
+		// String s7 = "122";
+		// Matcher m7 = p7.matcher(s7);
+		// p(m7.matches());
+
+		// flags的简写
+		// Pattern p8 = Pattern.compile("java", Pattern.CASE_INSENSITIVE);
 		// p("Java".matches("(?i)(java)"));
 	}
 
 	public static void p(Object o) {
 		System.out.println(o);
-		System.out.println("**********************");
-
+		System.out.println("*****************" + count++);
 	}
 
 }
