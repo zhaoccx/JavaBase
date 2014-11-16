250fa17c879c122e0991773946cbe213c3ef23a4
zhaoccx <zhaoccx@hotmail.com>
2014-11-17 00:27:41 +0800

    导入啊。

--- PatientInfo/src/org/com/util/DbUtil.java
+++ PatientInfo/src/org/com/util/DbUtil.java
@@ -5,36 +5,37 @@ import java.sql.DriverManager;
 
 public class DbUtil {
 
-	private String dbUrl="jdbc:mysql://localhost:3306/db_patient?useUnicode=true&characterEncoding=utf8";
-	private String dbUserName="root";
-	private String dbPassword="123456";
-	private String jdbcName="com.mysql.jdbc.Driver";
-	
+	private String dbUrl = "jdbc:mysql://localhost:3306/db_patient?useUnicode=true&characterEncoding=utf8";
+	private String dbUserName = "root";
+	private String dbPassword = "5487102";
+	private String jdbcName = "com.mysql.jdbc.Driver";
+
 	/**
 	 * 链接数据库
 	 *
 	 * @return
 	 * @throws Exception
 	 */
-	public Connection getCon() throws Exception{
+	public Connection getCon() throws Exception {
 		Class.forName(jdbcName);
-		Connection con=DriverManager.getConnection(dbUrl,dbUserName,dbPassword);
+		Connection con = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
 		return con;
 	}
-	
+
 	/**
 	 * 关闭数据库
+	 * 
 	 * @param con
 	 * @throws Exception
 	 */
-	public void closeCon(Connection con) throws Exception{
-		if(con!=null){
+	public void closeCon(Connection con) throws Exception {
+		if (con != null) {
 			con.close();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		DbUtil dbUtil=new DbUtil();
+		DbUtil dbUtil = new DbUtil();
 		try {
 			dbUtil.getCon();
 			System.out.println("��ݿ����ӳɹ�");
