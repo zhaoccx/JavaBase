18df725b5a6ab5c4d4a5f148c069ee45ad612fab
zhaoccx <zhaoccx@hotmail.com>
2014-12-18 18:21:03 +0800

    输入两个整数 n 和m，从数列1，2，3.......n中 随意取几个数, 使其和等于 m ,要求将其中所有的可能组合列出来.

---
+++ javaenhance/src/cn/itcast/day3/TestClone.java
@@ -0,0 +1,38 @@
+package cn.itcast.day3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestClone {
+
+	/**
+	 * 输入两个整数 n 和m，从数列1，2，3.......n中 随意取几个数, 使其和等于 m ,要求将其中所有的可能组合列出来.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	public List<Integer[]> getIntegersList(int n, int m) {
+		List<Integer[]> list = new ArrayList<Integer[]>();
+
+		return null;
+	}
+
+	public Map<Integer, Integer> getOtherMap(int n, int m) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int[] array = new int[n];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
+		}
+
+		while (m == n) {
+
+		}
+
+		return null;
+	}
+}
