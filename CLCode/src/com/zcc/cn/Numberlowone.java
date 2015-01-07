352cd8bcaa193da07496a135f016ced646a0f4bd
zhaoccx <zhaoccx@hotmail.com>
2015-01-07 12:17:34 +0800

    ssss

--- CLCode/src/com/zcc/cn/Numberlowone.java
+++ CLCode/src/com/zcc/cn/Numberlowone.java
@@ -3,29 +3,28 @@ package com.zcc.cn;
 public class Numberlowone {
 	public static void main(String[] args) {
 
-		sysCode("1aaeb20d831cf710");
+		numberAddsome("1aaeb20d831cf710", 5);
 
 	}
 
-	public static void sysCode(String strs) {
+	public static void numberAddsome(String strs, int some) {
 		StringBuffer buff = new StringBuffer();
 		char[] charArray = strs.toCharArray();
 		for (int i = 0; i < charArray.length; i++) {
 			try {
 				int s = Integer.parseInt(charArray[i] + "");
-				if (s == 0) {
-					s = 9;
-				} else if (s == 9) {
-					s = 0;
-				}else{
-					s++;
+				s += some;
+				s %= 10;
+				if (s < 0) {
+					s += 10;
 				}
+				
 				buff.append(s);
 			} catch (Exception e) {
 				buff.append(charArray[i]);
 			}
 		}
-		
+
 		System.out.println(buff.toString());
 
 	}
