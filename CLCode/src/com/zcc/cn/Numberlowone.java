564c52f2abd7bddfc4f1c7b530ac9466d8a95550
zhaoccx <zhaoccx@hotmail.com>
2015-01-07 13:09:25 +0800

    885

--- CLCode/src/com/zcc/cn/Numberlowone.java
+++ CLCode/src/com/zcc/cn/Numberlowone.java
@@ -7,6 +7,14 @@ public class Numberlowone {
 
 	}
 
+	/**
+	 * 数字加某一个加some;
+	 * 
+	 * @param strs
+	 *            源字符
+	 * @param some
+	 *            要加的数字
+	 */
 	public static void numberAddsomeLists(String strs, int some) {
 		char[] charArray = strs.toCharArray();
 		for (int i = 0; i < charArray.length; i++) {
